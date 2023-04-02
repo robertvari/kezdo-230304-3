614fd0ead0d285359d418e0a18dc519170ecfe12
Robert Vari <mail.robertvari@gmail.com>
2023-04-02 14:31:12 +0200

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -1,7 +1,9 @@
+import random
+
 class Player:
     def __init__(self):
-        self.__name = None
-        self.__credits = 0
+        self.__name = self.get_random_name()
+        self.__credits = random.randint(10, 100)
         self.__hand = []
         self.__playing = True
     
@@ -10,9 +12,15 @@ class Player:
         first_names = ["Marnie", "Johnathan", "Mahnoor", "Hassan", "Alissa", "Millie", "Qasim", "Damon", "Shreya", "Carly"]
         last_names =  ["Roy", "Aguirre", "Sandoval", "Rogers", "Cole", "Matthams", "Allen", "Stokes", "Deleon", "Hampton"]
 
+        return f"{random.choice(first_names)} {random.choice(last_names)}"
+
     def __str__(self) -> str:
         return f"Name: {self.__name}\nCredits: {self.__credits}\nCards: {self.__hand}"
 
 if __name__ == "__main__":
     player1 = Player()
+    player2 = Player()
+    player3 = Player()
     print(player1)
+    print(player2)
+    print(player3)
